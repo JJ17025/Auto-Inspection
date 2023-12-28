1e7bd86ad13b3e3c83de220216e054db0363e63e
pp <jaknarong.spk02@gmail.com>
2023-12-28 10:45:37 +0700

    modify update.py

--- update.py
+++ update.py
@@ -21,18 +21,21 @@ def update_a_program():
     result = subprocess.run('git status', capture_output=True, text=True)
     if result.stdout.split('\n')[1] == 'nothing to commit, working tree clean':
         print(GREEN, 'All files have not been changed.', ENDC, sep='')
-        r = requests.get('https://github.com/JJ17025/Auto-Inspection/commit/m2')
-        soup = BeautifulSoup(r.content, 'html.parser')
-        new_v = soup.find('span', {'class': 'sha user-select-contain'}).text
+        try:
+            r = requests.get('https://github.com/JJ17025/Auto-Inspection/commit/m2')
+            soup = BeautifulSoup(r.content, 'html.parser')
+            new_v = soup.find('span', {'class': 'sha user-select-contain'}).text
 
-        result = subprocess.run('git log --oneline', capture_output=True, text=True)
-        old_v = result.stdout[:7]
-        if new_v != old_v:
-            print(FAIL, 'old_v', old_v, ENDC, sep='')
-            print(GREEN, 'new_v', new_v, ENDC, sep='')
-            result = subprocess.run('git pull origin m2', capture_output=True, text=True)
-            print(result.stdout)
-        else:
-            print(GREEN, 'programe is the latest version.', ENDC, sep='')
+            result = subprocess.run('git log --oneline', capture_output=True, text=True)
+            old_v = result.stdout[:7]
+            if new_v != old_v:
+                print(FAIL, 'old_v', old_v, ENDC, sep='')
+                print(GREEN, 'new_v', new_v, ENDC, sep='')
+                result = subprocess.run('git pull origin m2', capture_output=True, text=True)
+                print(result.stdout)
+            else:
+                print(GREEN, 'programe is the latest version.', ENDC, sep='')
+        except:
+            print(FAIL, 'request to github error', ENDC, sep='')
     else:
         print(WARNING, 'file have been changed.', ENDC, sep='')
